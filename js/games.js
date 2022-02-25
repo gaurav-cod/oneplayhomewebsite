aea99d8f872c4b7963bfe1fa7a5951e402eb0d09
maynak <mainak@oneplay.in>
2022-02-25 06:17:45 -0800

    mobile responsive

--- js/games.js
+++ js/games.js
@@ -15,21 +15,20 @@ loadGames().then((data) => {
   const listEl = document.querySelector("#games-list");
   data.forEach((game) => {
     const gameEl = document.createElement("div");
-    gameEl.className = "col-sm-12 col-md-5 col-lg-3 mb-4";
+    gameEl.className = "col-sm-12 col-md-6 col-lg-4 col-xxl-3 mb-4";
     gameEl.innerHTML = `
-    <a class="card card-block border-0">
+    <div class="d-flex flex-column align-items-center">
       <img
-        class="card-img"
         src="${
           config.GAMES_ASSETS_PREFIX +
           game.oplay_id +
           game.text_background_image
         }"
-        height="153"
-        width="219"
+        height="170"
+        width="280"
         alt=""
       />
-    </a>
+    </div>
     `;
     listEl.appendChild(gameEl);
   });
