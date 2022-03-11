e8a89da7df30810da7a9309c7cb780f14e902f54
maynak <mainak@oneplay.in>
2022-03-11 18:22:29 +0530

    ttts

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.config = {
     SIGNUP_URL: 'http://oneplay.in/speed-test/?redirect=signup',
-    GAMES_API: 'https://rendermix.oneplay.in/v1/api/games',
+    GAMES_API: 'https://rendermix.oneplay.in/v1/games',
     APP_URL: 'https://oneplay.in/dashboard',
     MACOS_DOWNLOAD_URL: 'https://cdn.edge-net.co/clients/latest/mac_client.dmg',
     WINDOWS_DOWNLOAD_URL: 'https://cdn.edge-net.co/clients/latest/windows_client.exe',
