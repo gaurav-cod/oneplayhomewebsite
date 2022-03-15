ffa26e0814e9bf4947a5792cb505de7c6ff3ffcb
maynak <mainak@oneplay.in>
2022-03-15 17:25:26 +0530

    gffa

--- js/home.js
+++ js/home.js
@@ -1,3 +1,23 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie("op_session_token")) {
+  window.location.href = "/dashboard";
+}
+
 function loadGames() {
   return fetch(config.GAMES_API + "?page=0&limit=30", {
     headers: {
