d51fade6d35583036ac9dfdd85438e85cfcab4f5
dev-b-akratech <81217388+dev-b-akratech@users.noreply.github.com>
2023-02-07 07:31:43 +0530

    remove default spatial navigation focus

--- js/spatial-navigation.js
+++ js/spatial-navigation.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
   // Initialize
-  SpatialNavigation.init();
+  // SpatialNavigation.init();
 
   // Define navigable elements (anchors and elements with "focusable" class).
   SpatialNavigation.add({
@@ -8,17 +8,22 @@ window.addEventListener('load', function() {
   });
 
   // Make the *currently existing* navigable elements focusable.
-  SpatialNavigation.makeFocusable();
+  // SpatialNavigation.makeFocusable();
 
   // Focus the first navigable element.
-  SpatialNavigation.focus();
+  // SpatialNavigation.focus();
 });
 
 $.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
-$('a, .btn')
+$('input, select, textarea, a, button')
   .SpatialNavigation()
-  .focus(function() { $(this).css('outline', '2px solid white'); })
-  .blur(function() { $(this).css('outline', ''); })
-  .first()
-  .focus();
+  .focus(function() 
+    { 
+      $(this).css('outline', '2px solid white'); 
+    })
+  .blur(function() { 
+      $(this).css('outline', ''); 
+    })
+  // .first();
+  // .focus();
 });
