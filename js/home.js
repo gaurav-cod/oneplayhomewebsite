cd99460813a67204e8dcf54c813dfe16a024ce08
BurnedOx666 <mainak@oneplay.in>
2023-05-16 18:49:48 +0530

    remove on load

--- js/home.js
+++ js/home.js
@@ -1,10 +1,5 @@
-function checkSessionToken() {
-  const cookie = document.cookie.match(/op_session_token=(.*?);/);
+const cookie = document.cookie.match(/op_session_token=(.*?);/);
 
-  if (cookie) {
-    window.location.href = "/dashboard/";
-  }
+if (cookie) {
+  window.location.href = "/dashboard/";
 }
-
-// Execute the checkSessionToken function before the HTML loads.
-window.addEventListener("load", checkSessionToken);
