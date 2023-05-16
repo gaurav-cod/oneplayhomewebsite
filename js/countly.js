032f6c9c7bdaec0d76c27ea681d5a15572f27487
dev-b-akratech <81217388+dev-b-akratech@users.noreply.github.com>
2023-05-16 11:44:47 +0530

    countly integrated in website with few new features

--- js/countly.js
+++ js/countly.js
@@ -6,17 +6,22 @@ if(config.HAS_COUNTLY === "true") {
   //provide countly initialization parameters
   Countly.app_key = config.COUNTLY_APP_KEY;
   Countly.url = config.COUNTLY_URL;
+  Countly.heatmap_whitelist = config.BASE_URL;
   Countly.debug = true;
-  Countly.app_version = "1.2";
+  Countly.offline_mode = true;
+  Countly.remote_config = true;
+  // Countly.app_version = "1.2";
 
   Countly.q.push(['track_sessions']);
-  Countly.q.push(['track_pageview']);
-  // Countly.q.push(['track_clicks']);
+  Countly.q.push(['track_pageview',location.pathname+location.hash]);
+  Countly.q.push(['track_clicks']);
   Countly.q.push(['track_scrolls']);
   Countly.q.push(['track_errors']);
-  // Countly.q.push(['track_links']);
-  Countly.q.push(['track_forms']);
+  Countly.q.push(['track_links']);
   Countly.q.push(['collect_from_forms']);
+
+  //will collect hidden inputs
+  Countly.q.push(['track_forms', null, true]);
 
   //automatically report traces
   Countly.q.push(["track_performance", {
@@ -53,6 +58,9 @@ if(config.HAS_COUNTLY === "true") {
 
   //load countly script asynchronously
   (function() {
+    $(window).on('hashchange', function() {
+      Countly.q.push(['track_pageview',location.pathname+location.hash]);
+    });
     var cly = document.createElement('script'); cly.type = 'text/javascript';
     cly.async = true;
     //enter url of script here
