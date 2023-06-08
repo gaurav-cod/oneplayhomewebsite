6f124dcbdc79e35fc7482f9fde280303b98c55bd
dev-b-akratech <81217388+dev-b-akratech@users.noreply.github.com>
2023-06-08 10:12:33 +0530

    Countly track performance code add but still it not working in countly dashboard

--- js/countly.js
+++ js/countly.js
@@ -1,3 +1,28 @@
+syncScripts();
+function syncScripts() {
+  // please provide the correct path to these files according to your project structure
+  var scripts = ['./js/countly_boomerang.js', 'https://cdn.jsdelivr.net/npm/boomerangjs@1.0.1/boomerang.min.js'];
+  var i = 0;
+  function loopScriptList(scripts) {
+      recursiveScriptMaker(scripts[i], function() {
+          i++;
+          if(i < scripts.length) {
+              loopScriptList(scripts);   
+          }
+      }); 
+  }
+  loopScriptList(scripts);      
+}
+function recursiveScriptMaker(source, callback ) {
+  var script = document.createElement('script');
+  script.onload = function() {
+      console.log('Successfully loaded the source: ' + source)
+      callback();
+  }
+  script.src = source;
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 //some default pre init
 var Countly = Countly || {};
 Countly.q = Countly.q || [];
@@ -7,6 +32,7 @@ Countly.app_key = config.COUNTLY_APP_KEY;
 Countly.url = config.COUNTLY_URL;
 Countly.heatmap_whitelist = config.BASE_URL;
 Countly.app_version = config.APP_VERSION;
+Countly.debug = true;
 
 Countly.q.push(['track_sessions']);
 // Countly.q.push(['track_pageview',location.pathname+location.hash]);
@@ -19,6 +45,7 @@ Countly.q.push(['collect_from_forms']);
 //will collect hidden inputs
 Countly.q.push(['track_forms', null, true]);
 
+//automatically report traces
 Countly.q.push(["track_performance", {
   //page load timing
   RT:{},
@@ -56,15 +83,18 @@ function countlyEvent(ob){
 
 //load countly script asynchronously
 (function() {
+  
   $(window).on('hashchange', function() {
     Countly.q.push(['track_pageview',location.pathname+location.hash]);
   });
+  
   var cly = document.createElement('script'); cly.type = 'text/javascript';
   cly.async = true;
   //enter url of script here
   cly.src = config.COUNTLY_SRC;
   cly.onload = function(){
-    Countly.init()   
+    Countly.init();
   };
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cly, s);
+  var s = document.getElementsByTagName('script')[0]; 
+  s.parentNode.insertBefore(cly, s);
 })();
