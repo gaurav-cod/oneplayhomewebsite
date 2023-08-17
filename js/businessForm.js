70e08680ae50b652f4dd6bfd4c4d406afb7ece93
Maynak <mainak@oneplay.in>
2023-08-17 12:34:51 +0000

    2.5.0

--- js/businessForm.js
+++ js/businessForm.js
@@ -1,66 +1,130 @@
 const thisForm = document.getElementById("businessData");
+document.getElementById("submitBtn").classList.add("disabled");
 
-thisForm.addEventListener("submit", async function (e) {
-  e.preventDefault();
+const nameField = document.forms["businessData"]["name"];
+const emailField = document.forms["businessData"]["email"];
+const phoneField = document.forms["businessData"]["phone"];
+const websiteField = document.forms["businessData"]["website"];
+const companyDetailField = document.forms["businessData"]["company_detail"];
+const partnershipField = document.forms["businessData"]["partnership"];
 
-  let name = document.forms["businessData"]["name"].value;
-  let email = document.forms["businessData"]["email"].value;
-  let phone = document.forms["businessData"]["phone"].value;
-  let website = document.forms["businessData"]["website"].value;
-  let companyDetail = document.forms["businessData"]["company_detail"].value;
-  let partnership = document.forms["businessData"]["partnership"].value;
+const namePattern = /^([a-zA-Z]+\s?[a-zA-Z]+)+$/i;
+const phonePattern = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const linkPattern =
+  /\b(?:https?|ftp):\/\/[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]/;
 
-  const namePattern = /^[a-zA-Z\s]*$/;
-  const phonePattern = /^\+(?:[0-9] ?){6,14}[0-9]$/;
-  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  const linkPattern =
-    /\b(?:https?|ftp):\/\/[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]/;
-
-  let isValid = true;
-  if (!name.match(namePattern)) {
+const isNameValid = () => {
+  if (!nameField.value.match(namePattern)) {
     document.getElementById("invalidName").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidName").classList.add("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidName").classList.add("d-none");
+    return true;
+  }
+}
 
-  if (!phone.match(phonePattern)) {
-    document.getElementById("invalidPhone").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidPhone").classList.add("d-none");
-
-  if (!email.match(emailPattern)) {
+const isEmailValid = () => {
+  if (!emailField.value.match(emailPattern)) {
     document.getElementById("invalidEmail").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidEmail").classList.add("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidEmail").classList.add("d-none");
+    return true;
+  }
+}
 
-  if (!website.match(linkPattern)) {
+const isPhoneValid = () => {
+  if (!phoneField.value.match(phonePattern)) {
+    document.getElementById("invalidPhone").classList.remove("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidPhone").classList.add("d-none");
+    return true;
+  }
+}
+
+const isWebsiteFieldValid = () => {
+  if (!websiteField.value.match(linkPattern)) {
     document.getElementById("invalidWebsite").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidWebsite").classList.add("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidWebsite").classList.add("d-none");
+    return true;
+  }
+}
 
-  if (companyDetail === "") {
+const isCompanyDetailFieldValid = () => {
+  if (companyDetailField.value === "") {
     document.getElementById("invalidCompanyDetail").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidCompanyDetail").classList.add("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidCompanyDetail").classList.add("d-none");
+    return true;
+  }
+}
 
-  if (partnership === "") {
+const isPartnershipFieldValid = () => {
+  if (partnershipField.value === "") {
     document.getElementById("invalidPartnership").classList.remove("d-none");
-    isValid = false;
-  } else document.getElementById("invalidPartnership").classList.add("d-none");
+    return false;
+  } else {
+    document.getElementById("invalidPartnership").classList.add("d-none");
+    return true;
+  }
+}
 
-  if (isValid) {
+const validateFields = () => {
+  let isValid = true;
+  
+  isValid = isValid && isNameValid();
+  isValid = isValid && isEmailValid();
+  isValid = isValid && isPhoneValid();
+  isValid = isValid && isWebsiteFieldValid();
+  isValid = isValid && isCompanyDetailFieldValid();
+  isValid = isValid && isPartnershipFieldValid();
+
+  console.warn(isValid, 'sbt btn')
+  if (!isValid) {
+    document.getElementById("submitBtn").classList.add("disabled");
+  } else {
+    document.getElementById("submitBtn").classList.remove("disabled");
+  }
+
+  return isValid;
+}
+
+nameField.addEventListener("input", (e) => {
+  validateFields();
+})
+phoneField.addEventListener("input", (e) => {
+  validateFields();
+})
+websiteField.addEventListener("input", (e) => {
+  validateFields();
+})
+emailField.addEventListener("input", (e) => {
+  validateFields();
+})
+companyDetailField.addEventListener("input", (e) => {
+  validateFields();
+})
+partnershipField.addEventListener("input", (e) => {
+  validateFields();
+})
+
+thisForm.addEventListener("submit", function (e) {
+  if (validateFields()) {
+    e.preventDefault();
     const formData = new FormData();
+    formData.append("Name_First", nameField);
+    formData.append("Email", emailField);
+    formData.append("PhoneNumber_countrycode", phoneField);
+    formData.append("Website", websiteField);
+    formData.append("SingleLine", companyDetailField);
+    formData.append("MultiLine", partnershipField);
 
-    const [fisrtName, ...lastName] = name.trim().split(" ");
-
-    formData.append("Name_First", fisrtName);
-    formData.append("Name_Last", lastName?.join(" ") || "");
-    formData.append("Email", email);
-    formData.append("PhoneNumber_countrycode", phone);
-    formData.append("Website", website);
-    formData.append("SingleLine", companyDetail);
-    formData.append("MultiLine", partnership);
-
-    var requestOptions = {
+    let requestOptions = {
       method: "POST",
       body: formData,
     };
@@ -68,11 +132,7 @@ thisForm.addEventListener("submit", async function (e) {
     document.getElementById("successResponse").classList.remove("d-none");
     document.getElementById("defaultForm").classList.add("d-none");
 
-    try {
-      await fetch(window.config.BUSINESS_FORM, requestOptions);
-    } catch (e) {
-      console.log(e);
-    }
+    fetch(window.config.BUSINESS_FORM, requestOptions).catch(console.log)
   }
 
   return false;
