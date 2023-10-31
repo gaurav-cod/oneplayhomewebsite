8897bf52c8cf5da11264a3210ef4283e1c97a5e6
Ajay Tambe <ajay@oneplay.in>
2023-10-31 20:29:24 +0530

    WEB-930 hotfix for Upto text on 4k and 1080p plans

--- js/subscription.js
+++ js/subscription.js
@@ -21,9 +21,9 @@ function loadSubscriptions() {
  */
 function getResolution(sub) {
     if (sub['can_run_4k'] == 'true') {
-        return '4K';
+        return '<span class="font8">Upto</span> 4K';
     } else if (sub['can_run_hd'] == 'true') {
-        return '1080p';
+        return '<span class="font8">Upto</span> 1080p';
     } else {
         return '720p';
     }
